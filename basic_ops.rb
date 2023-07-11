006fe84db949acff0c64128e955262e0d90cc733
Fezzopro <fezzopro@gmail.com>
2023-07-11 09:29:26 +0200

    create the first ruby file

---
+++ basic_ops.rb
@@ -0,0 +1,55 @@
+print "Enter the first number: "
+
+first_number = gets.to_i
+
+print "Enter the second number: "
+
+second_number = gets.to_i
+
+puts first_number.to_s + " + " + second_number.to_s + " = " + (first_number + second_number).to_s
+
+# Arithmetic operations
+
+puts "6 + 4 = " + (6 + 4).to_s
+puts "6 - 4 = " + (6 - 4).to_s
+puts "6 * 4 = " + (6 * 4).to_s
+puts "6 / 4 = " + (6 / 4).to_s
+puts "6 % 4 = " + (6 % 4).to_s
+
+# float accuracy
+
+num_one = 1.000
+
+num99 = 0.999
+
+puts num_one.to_s + " - " + num99.to_s + " = " + (num_one - num99).to_s
+
+# everything in ruby is an object
+
+puts 1.class
+puts 1.990.class
+puts "string".class
+
+# constants in Ruby are writter in capital letters
+
+A_CONSTANT = 3.14
+
+A_CONSTANT = 3.141  # this will throw a warning
+
+puts A_CONSTANT
+
+# Using File object
+
+write_handler = File.new("random.out", "w") # create a file
+
+write_handler.puts("Random text") # write text to that file
+
+write_handler.close # close the file
+
+data_from_file = File.read("random.out") # read from a file
+
+puts "Data from a file: " + data_from_file
+
+# Execute codes from other ruby files
+
+load "rubyOrNotRubyFile.rb"
