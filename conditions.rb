a51be9743658bb3b16dd572a87a14186aa4ed4e3
Fezzopro <fezzopro@gmail.com>
2023-07-25 23:26:33 +0200

    Add if and if_else statements

---
+++ conditions.rb
@@ -0,0 +1,17 @@
+# if and if else statements
+is_male = true
+
+if is_male
+  puts "He's a male"
+end
+
+# proper way when it's a one line condition
+
+puts "He's a male" if is_male
+
+is_male = false
+if is_male
+  puts "He's a male"
+else
+  puts "She's a female"
+end
