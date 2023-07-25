d457ecdbcd02b03344d458ed566ba43bdfe088b1
Fezzopro <fezzopro@gmail.com>
2023-07-25 22:34:23 +0200

    Refactor basic_ops.rb

--- basic_ops.rb
+++ basic_ops.rb
@@ -1,20 +1,20 @@
-print "Enter the first number: "
+print 'Enter the first number: '
 
 first_number = gets.to_i
 
-print "Enter the second number: "
+print 'Enter the second number: '
 
 second_number = gets.to_i
-
-puts first_number.to_s + " + " + second_number.to_s + " = " + (first_number + second_number).to_s
+sum = (first_number + second_number).to_s
+result first_number.to_s + ' + ' + second_number.to_s + ' = ' + sum
 
 # Arithmetic operations
 
-puts "6 + 4 = " + (6 + 4).to_s
-puts "6 - 4 = " + (6 - 4).to_s
-puts "6 * 4 = " + (6 * 4).to_s
-puts "6 / 4 = " + (6 / 4).to_s
-puts "6 % 4 = " + (6 % 4).to_s
+puts '6 + 4 = ' + (6 + 4).to_s
+puts '6 - 4 = ' + (6 - 4).to_s
+puts '6 * 4 = ' + (6 * 4).to_s
+puts '6 / 4 = ' + (6 / 4).to_s
+puts '6 % 4 = ' + (6 % 4).to_s
 
 # float accuracy
 
@@ -22,34 +22,34 @@ num_one = 1.000
 
 num99 = 0.999
 
-puts num_one.to_s + " - " + num99.to_s + " = " + (num_one - num99).to_s
+puts num_one.to_s + ' - ' + num99.to_s + ' = ' + (num_one - num99).to_s
 
 # everything in ruby is an object
 
 puts 1.class
 puts 1.990.class
-puts "string".class
+puts 'string'.class
 
 # constants in Ruby are written in capital letters
 
 A_CONSTANT = 3.14
 
-A_CONSTANT = 3.141  # this will throw a warning
+A_CONSTANT = 3.141 # this will throw a warning
 
 puts A_CONSTANT
 
 # Using File object
 
-write_handler = File.new("random.out", "w") # create a file
+write_handler = File.new('random.out', 'w') # create a file
 
-write_handler.puts("Random text") # write text to that file
+write_handler.puts('Random text') # write text to that file
 
 write_handler.close # close the file
 
-data_from_file = File.read("random.out") # read from a file
+data_from_file = File.read('random.out') # read from a file
 
-puts "Data from a file: " + data_from_file
+puts 'Data from a file: ' + data_from_file
 
 # Execute codes from other ruby files
 
-load "rubyOrNotRubyFile.rb"
+load 'rubyOrNotRubyFile.rb'
