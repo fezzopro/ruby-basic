e560c31ac2a6ee87070a70317c78f77a8a02ca15
Fezzopro <fezzopro@gmail.com>
2023-08-20 20:16:08 +0200

    Ruby program to illustrate the Boolean Data Type

--- data_types.rb
+++ data_types.rb
@@ -19,3 +19,22 @@ time = 9.87 / 3600
 speed = distance / time
 puts "The average speed of a sprinter is #{speed} km/h"
 
+# Ruby program to illustrate the Boolean Data Type
+
+if true
+  puts "It is True!"
+else
+  puts "It is False!"
+end
+
+if nil
+  puts "nil is True!"
+else
+  puts "nil is False!"
+end
+
+if 0
+  puts "0 is True!"
+else
+  puts "0 is False!"
+end
