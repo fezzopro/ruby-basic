f2521b4c7ee6e0ae028e95d8d8e6b87059d03ba7
Fezzopro <fezzopro@gmail.com>
2023-08-20 20:15:48 +0200

    Ruby program to illustrate the Numbers Data Type

---
+++ data_types.rb
@@ -0,0 +1,21 @@
+=begin
+  Data types in Ruby represents different types of data like text, string, numbers, etc. All data types are based on classes because it is a pure Object-Oriented language. There are different data types in Ruby as follows:
+
+  Numbers
+  Boolean
+  Strings
+  Hashes
+  Arrays
+  Symbols
+  
+=end
+# Ruby program to illustrate the Numbers Data Type
+
+# float type
+distance = 0.1
+
+# both integer and float type
+time = 9.87 / 3600
+speed = distance / time
+puts "The average speed of a sprinter is #{speed} km/h"
+
