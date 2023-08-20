58f13df6097ca7f790093783f1beeb88db2e1e9a
Fezzopro <fezzopro@gmail.com>
2023-08-20 20:20:23 +0200

    Ruby program to illustrate the Hashes Data Type

--- data_types.rb
+++ data_types.rb
@@ -44,3 +44,11 @@ puts('--------------String-------------')
 puts "String Data Type";
 puts 'escape using "\\"';
 puts 'That\'s right';
+
+# Ruby program to illustrate the Hashes Data Type
+
+puts('--------------Hashes-------------')
+hsh = colors = { "red" => 0xf00, "green" => 0x0f0, "blue" => 0x00f }
+hsh.each do |key, value|
+print key, " is ", value, "\n"
+end
